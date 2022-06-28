e3f5e28a28accbdd5c9269ae639a876e105e362d
FilipApa <filipapatovic992@gmail.com>
2022-06-28 21:56:36 +0200

    caroseul

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
                     </main>
                 </div>
         <footer>
+            
             <?php wp_footer(); ?>
-            <p>Footer</p>
         </footer>
     </body>
 </html>
--- single.php
+++ single.php
@@ -1,3 +1,4 @@
+<?php get_header(); ?>
 <div class="container">
     <?php 
         while(have_posts()) {
@@ -5,29 +6,34 @@
             <div class="post-navigation">
                 <?php 
                 $category = get_the_category(); 
-                $cat_id = $category["0"]->term_id;
+                if($category) { $cat_id = $category["0"]->term_id;}
+               
                 ?>  
-                <a href="<?php echo home_url() ?>"> Home | </a>   
-                <a href="<?php echo get_category_link( $cat_id ); ?>"> 
-                    <?php echo $category["0"]->cat_name . '|'; ?>
-                </a>
-                <span>
-                    <?php the_title(); ?>
-                </span>
-                
+                <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <a href="<?php echo home_url() ?>"> Home  </a>  
+                        </li>
+                        <li class="breadcrumb-item">
+                            <a href="<?php echo get_category_link( $cat_id ); ?>"> 
+                                <?php echo $category["0"]->cat_name; ?>
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page"><?php the_title(); ?></li>
+                    </ol>
+                </nav>   
             </div>
             <div class="post-wrapper">
                 <div class="post-image">
                     <?php the_content(); ?>
                 </div>
 
-                <div>
-                    <?php previous_post_link( '%link', 'Prev post in category', true );
-                    next_post_link( '%link', 'Next post in category', true );?>
-             
-
+                <div class="d-flex justify-content-between align-items-center">
+                    <?php previous_post_link('%link', '<button type="button" class="btn btn-primary">PREV</button>', true );?>
+                    <?php next_post_link('%link', '<button type="button" class="btn btn-primary">NEXT</button>', true );?>
                 </div>
             </div>
         
         <?php } ?>
 </div>
+<?php get_footer(); ?>
