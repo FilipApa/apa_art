1af3886670067208ebb11db6fb71a97b0874349a
FilipApa <filipapatovic992@gmail.com>
2022-07-10 00:13:18 +0200

    filter section html

--- page-template/template-display-art.php
+++ page-template/template-display-art.php
@@ -41,73 +41,75 @@ if ($slug) {
                     Filter 
                 </button>   
             </p>
-            <div class="collapse" id="collapseExample">
-                <div class="card card-body d-flex flex-sm-column flex-md-row ">
-                    <div class="me-4">
-                        <h3 class="fs-5">Year</h3>
-                        <div>
-                            <?php 
-                                $args=array(
-                                'name'    => 'year',
-                                'public'   => true,
-                                '_builtin' => false
-                                );
-                                $output = 'names'; // or objects
-                                $operator = 'and';
-                                $taxonomies=get_taxonomies($args,$output,$operator); 
-
-                                if  ($taxonomies) {
-                                foreach ($taxonomies  as $taxonomy ) {
-                                    $terms = get_terms([
-                                        'taxonomy' => $taxonomy,
-                                        'hide_empty' => false,
-                                    ]);
-                    
-                                        foreach ( $terms as $term) {
-                                ?>
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" name="<?php echo esc_html($term->name); ?>" value="<?php echo esc_html($term->name); ?>" id="flexCheckDefault">
-                                            <label class="form-check-label" for="flexCheckDefault">
-                                            <?php echo esc_html($term->name); ?>
-                                            </label>
-                                        </div>
-                                <?php }}}?> 
-                        </div>  
-                    </div>
-                    
-                    <div>
-                        <h3 class="fs-5">Series</h3>
-                        <div>
-                            <?php 
-                            $args=array(
-                            'name'    => 'serie',
-                            'public'   => true,
-                            '_builtin' => false
-                            );
-                            $output = 'names'; 
-                            $operator = 'and';
-                            $taxonomies=get_taxonomies($args,$output,$operator); 
-            
-                            if  ($taxonomies) {
-                            foreach ($taxonomies  as $taxonomy ) {
-                                $terms = get_terms([
-                                    'taxonomy' => $taxonomy,
-                                    'hide_empty' => false,
-                                ]);
-                
-                                    foreach ( $terms as $term) {
-                            ?>
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" name="<?php echo esc_html($term->name); ?>" value="<?php echo esc_html($term->name); ?>" id="flexCheckDefault">
-                                <label class="form-check-label" for="flexCheckDefault">
-                                <?php echo esc_html($term->name); ?>
-                                </label>
-                            </div>
-                                
-                            <?php }}}?> 
-                        </div>
+            <div class="collapse " id="collapseExample">
+                <div class="card card-body d-flex flex-sm-column  ">
+                    <div class=" me-4">
+                        <a class="btn btn-outline-dark mb-2" data-bs-toggle="collapse" href="#collapseYear" role="button" aria-expanded="false" aria-controls="collapseYear">
+                            Year
+                        </a>
                         
-                    </div> 
+                        <div class="collapse" id="collapseYear">
+                            <div class="card card-body  mb-2">
+                                <?php 
+                                    $args=array(
+                                    'name'    => 'year',
+                                    'public'   => true,
+                                    '_builtin' => false
+                                    );
+                                    $output = 'names'; // or objects
+                                    $operator = 'and';
+                                    $taxonomies=get_taxonomies($args,$output,$operator); 
+
+                                    if  ($taxonomies) {
+                                    foreach ($taxonomies  as $taxonomy ) {
+                                        $terms = get_terms([
+                                            'taxonomy' => $taxonomy,
+                                            'hide_empty' => false,
+                                        ]); foreach ( $terms as $term) {?>
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" name="<?php echo esc_html($term->name); ?>" value="<?php echo esc_html($term->name); ?>" id="flexCheckDefault">
+                                                <label class="form-check-label" for="flexCheckDefault">
+                                                <?php echo esc_html($term->name); ?>
+                                                </label>
+                                            </div>
+                                    <?php }}}?> 
+                                </div>
+                            </div>  
+                        </div>
+                    
+                        <div class=" me-4">
+                        <a class="btn btn-outline-dark mb-2" data-bs-toggle="collapse" href="#collapseSeries" role="button" aria-expanded="false" aria-controls="collapseSeries">
+                            Series
+                        </a>
+                        
+                        <div class="collapse" id="collapseSeries">
+                            <div class="card card-body  mb-2">
+                                <?php 
+                                    $args=array(
+                                    'name'    => 'serie',
+                                    'public'   => true,
+                                    '_builtin' => false
+                                    );
+                                    $output = 'names'; // or objects
+                                    $operator = 'and';
+                                    $taxonomies=get_taxonomies($args,$output,$operator); 
+
+                                    if  ($taxonomies) {
+                                    foreach ($taxonomies  as $taxonomy ) {
+                                        $terms = get_terms([
+                                            'taxonomy' => $taxonomy,
+                                            'hide_empty' => false,
+                                        ]); foreach ( $terms as $term) {?>
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" name="<?php echo esc_html($term->name); ?>" value="<?php echo esc_html($term->name); ?>" id="flexCheckDefault">
+                                                <label class="form-check-label" for="flexCheckDefault">
+                                                <?php echo esc_html($term->name); ?>
+                                                </label>
+                                            </div>
+                                    <?php }}}?> 
+                                </div>
+                            </div>  
+                        </div>
                 </div>
             </div>
         </div>
