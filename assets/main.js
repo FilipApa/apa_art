c4e51f7ccb9fb74ed6cefb00749bc0e3f070e898
FilipApa <filipapatovic992@gmail.com>
2022-07-13 19:58:57 +0200

    rest api filter by category

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,19 @@
+const container = document.getElementById('container');
+const websiteURl = container.dataset.websiteUrl;
+const postCategory = container.dataset.postCategory;
 
+console.log(websiteURl);
+console.log(postCategory);
+
+async function fetchCategoryPosts() {
+    let response = await fetch(`${websiteURl}/wp-json/wp/v2/categories?slug=${postCategory}`);
+    let data = await response.json();
+    let catID = data[0].id;
+ 
+    let getCatPost = await fetch(`${websiteURl}/wp-json/wp/v2/posts?categories=${catID}`);
+    let catData = await getCatPost.json();
+    console.log(catData);
+
+}
+
+fetchCategoryPosts();
--- page-template/template-display-art.php
+++ page-template/template-display-art.php
@@ -24,12 +24,12 @@ if ($slug) {
       $query = new WP_Query( $args ); 
 
 ?>
-    <div class="container">
+    <div class="container" id="container" data-post-category="<?php echo esc_html($slug); ?>" data-website-url="<?php echo home_url(); ?>" >
          <div class="post-navigation mb-4">
             <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item">
-                        <a href="<?php echo home_url() ?>"> Home </a>  
+                        <a href="<?php echo home_url(); ?>"> Home </a>  
                     </li>
                     <li class="breadcrumb-item active" aria-current="page">
                         <?php echo esc_html($current_page->name); ?>
@@ -122,7 +122,7 @@ if ($slug) {
             <?php 
             if ( $index % $no_of_columns === 0  ) {
                 ?>
-                <div class="row" id="template-grid-content">
+                <div class="row" id="template-grid-content" >
                 <?php } ?>
 
                     <div class="post col-lg-4 col-md-6 col-sm-12 d-flex flex-column align-items-center gy-4" >
