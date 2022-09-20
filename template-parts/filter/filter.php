0b0f735f17ef6284f2c4d73b65079c0adad88e46
FilipApa <filipapatovic992@gmail.com>
2022-09-20 21:04:33 +0200

    filter new html classes for css

--- template-parts/filter/filter.php
+++ template-parts/filter/filter.php
@@ -1,86 +1,79 @@
-<div>  
-    <button class="filter-btn" type="button">
+<div class="filter-btn" id="filter-section" role="button">  
         Filter
-    </button>   
 </div>
 
-<div class="">
+<div class="filter-dropdown">
     <div class="">
-        <div class="">
-            <a class="" role="button" >
+            <div class="filter-btn" id="filter-year" role="button" >
                 Year
-            </a>
+            </div>
 
-            <div class="">
-                <div class="">
-                    <?php 
-                        $args=array(
-                        'name'    => 'year',
-                        'public'   => true,
-                        '_builtin' => false,
-                        );
-                        // Get the page slug
-                        $slug = $GLOBALS['current_page']->slug;
+            <ul class="filter-dropdown">
+                <?php 
+                    $args=array(
+                    'name'    => 'year',
+                    'public'   => true,
+                    '_builtin' => false,
+                    );
+                    // Get the page slug
+                    $slug = $GLOBALS['current_page']->slug;
 
-                        $output = 'names'; 
-                        $operator = 'and';
-                        $taxonomies=get_taxonomies($args,$output,$operator); 
+                    $output = 'names'; 
+                    $operator = 'and';
+                    $taxonomies=get_taxonomies($args,$output,$operator); 
 
-                        if  ($taxonomies) {
-                        foreach ($taxonomies  as $taxonomy ) {
-                            $terms = get_terms([
-                                'taxonomy' => $taxonomy,
-                                'hide_empty' => false,
-                            ]); foreach ( $terms as $term) {?>
-                                <div class="form-check">
-                                    <input class="form-check-input form-check-input-year" type="checkbox" name="<?php echo esc_html($term->name); ?>" value="<?php echo esc_html($term->name); ?>" id="flexCheckDefault">
-                                    <label class="form-check-label" for="flexCheckDefault">
-                                        <?php echo esc_html($term->name); ?> 
-                                        <span class="">
-                                        <?php echo '(' . calc_num_of_posts($slug, 'year', $term->name) . ')'; ?>
-                                    </span>
-                                    </label>
-                                </div>
-                        <?php }}}?> 
-                    </div>
-                </div>  
+                    if  ($taxonomies) {
+                    foreach ($taxonomies  as $taxonomy ) {
+                        $terms = get_terms([
+                            'taxonomy' => $taxonomy,
+                            'hide_empty' => false,
+                        ]); foreach ( $terms as $term) {?>
+                            <li class="form-check">
+                                <input class="form-check-input form-check-input-year" type="checkbox" name="<?php echo esc_html($term->name); ?>" value="<?php echo esc_html($term->name); ?>" id="flexCheckDefault">
+                                <label class="form-check-label" for="flexCheckDefault">
+                                    <?php echo esc_html($term->name); ?> 
+                                    <span class="">
+                                    <?php echo '(' . calc_num_of_posts($slug, 'year', $term->name) . ')'; ?>
+                                </span>
+                                </label>
+                        </li>
+                    <?php }}}?> 
+                </ul>  
             </div>
         
             <div class="">
-            <a class=""  role="button" aria-expanded="false" aria-controls="">
-                Series
-            </a>
-            
-            <div class="">
-                <div class="">
-                    <?php 
-                        $args=array(
-                        'name'    => 'serie',
-                        'public'   => true,
-                        '_builtin' => false
-                        );
-                        $output = 'names';
-                        $operator = 'and';
-                        $taxonomies=get_taxonomies($args,$output,$operator); 
-                      
-                        if  ($taxonomies) {
-                        foreach ($taxonomies  as $taxonomy ) {
-                            $terms = get_terms([
-                                'taxonomy' => $taxonomy,
-                                'hide_empty' => false,
-                            ]); foreach ( $terms as $term) { ?>
-                                <div class="form-check">
-                                    <input class="form-check-input form-check-input-series" type="checkbox" name="<?php echo esc_html($term->name); ?>" value="<?php echo esc_html($term->slug); ?>" id="flexCheckDefault">
-                                    <label class="form-check-label" for="flexCheckDefault">
-                                    <?php echo esc_html($term->name); ?>
-                                    <span class="ms-1 text-secondary">
-                                        <?php echo '(' . calc_num_of_posts($slug, 'serie', $term->name) . ')'; ?>
-                                    </span>
-                                    </label>
-                                </div>
-                        <?php }}}?> 
-                    </div>
-                </div>  
+                <div class="filter-btn" id="filter-series" role="button" >
+                    Series
+                </div>
+                
+                <ul class="filter-dropdown">    
+                <?php 
+                    $args=array(
+                    'name'    => 'serie',
+                    'public'   => true,
+                    '_builtin' => false
+                    );
+                    $output = 'names';
+                    $operator = 'and';
+                    $taxonomies=get_taxonomies($args,$output,$operator); 
+                    
+                    if  ($taxonomies) {
+                    foreach ($taxonomies  as $taxonomy ) {
+                        $terms = get_terms([
+                            'taxonomy' => $taxonomy,
+                            'hide_empty' => false,
+                        ]); foreach ( $terms as $term) { ?>
+                            <li class="form-check">
+                                <input class="form-check-input form-check-input-series" type="checkbox" name="<?php echo esc_html($term->name); ?>" value="<?php echo esc_html($term->slug); ?>" id="flexCheckDefault">
+                                <label class="form-check-label" for="flexCheckDefault">
+                                <?php echo esc_html($term->name); ?>
+                                <span class="ms-1 text-secondary">
+                                    <?php echo '(' . calc_num_of_posts($slug, 'serie', $term->name) . ')'; ?>
+                                </span>
+                                </label>
+                            </li>
+                    <?php }}}?> 
+                </ul>  
             </div>
             <button class="" id="filterBtn">Filter</button>
     </div>
