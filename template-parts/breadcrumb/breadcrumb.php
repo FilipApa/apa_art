d5e7e6d85c7b08fbed379991b343502fc0104c75
FilipApa <filipapatovic992@gmail.com>
2022-09-25 13:01:34 +0200

    navigation breadcrumb

--- template-parts/breadcrumb/breadcrumb.php
+++ template-parts/breadcrumb/breadcrumb.php
@@ -3,7 +3,10 @@
 <nav aria-label="breadcrumb">
     <ul>
         <li class="breadcrumb-item">
-            <a href="<?php echo home_url(); ?>"> Home /</a>  
+            <a href="<?php echo home_url(); ?>"> 
+                <img src="<?php echo get_theme_file_uri( './assets/images/home.svg' ) ?>" alt="Home logo icon">
+            </a>  
+            <img class="breadcrumb-divider" src="<?php echo get_theme_file_uri( './assets/images/arrow-right.svg' ) ?>" alt="Right arrow icon">
         </li>
         <li class="breadcrumb-item" aria-current="page">
             <h1 class="breadcrumb-title">    
