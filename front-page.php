5b8dbe588c79d8ed6ade580312b0079831e04472
FilipApa <filipapatovic992@gmail.com>
2022-10-19 20:15:23 +0200

    home page html

--- front-page.php
+++ front-page.php
@@ -5,8 +5,61 @@
 
     </section>
 
-    <section class="testemonials">
-        
+    <section class="art-selection">
+        <div class="blue">
+            <div class="art-selection-background">
+                <picture>
+                    <source media="(min-width:720px)" srcset="">
+                    <img src="" alt="">
+                </picture>
+                </div>
+                
+                <div class="art-selection-text">
+                <div>
+                    <img src="" alt="">
+                </div>
+
+                <h2>Paintings</h2>
+            
+                <a href="#">See more</a>
+            </div>
+        </div>
+        <div class="flex-reverse red">
+            <div class="art-selection-background">
+                <picture>
+                    <source media="(min-width:720px)" srcset="">
+                    <img src="" alt="">
+                </picture>
+                </div>
+                
+                <div class="art-selection-text">
+                <div>
+                    <img src="" alt="">
+                </div>
+                
+                <h2>Digital art</h2>
+            
+                <a href="#">See more</a>
+            </div>
+        </div>
+        <div class="flex-reverse yellow">
+            <div class="art-selection-background">
+                <picture>
+                    <source media="(min-width:720px)" srcset="">
+                    <img src="" alt="">
+                </picture>
+                </div>
+                
+                <div class="art-selection-text">
+                <div>
+                    <img src="" alt="">
+                </div>
+
+                <h2>3D Animations</h2>
+            
+                <a href="#">See more</a>
+            </div>
+        </div>
     </section>
 </div>
 <?php  get_footer(); ?>
